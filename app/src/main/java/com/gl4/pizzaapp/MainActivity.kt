ba9f09b9720e72b7b0b66bcfb2a6529d48b72caa
safa-abidi <safa.abidi99@gmail.com>
2022-10-05 18:56:38 +0100

    initial commit

---
+++ app/src/main/java/com/gl4/pizzaapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gl4.pizzaapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
